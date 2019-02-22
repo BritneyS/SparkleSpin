c010fcd09884c842cdb607b06b511e0177d0fd91
Britney Smith <brit.smith7@gmail.com>
2019-02-22 09:49:10 -0500

    Add correct font names

--- SparkleSpin/Style/Font.swift
+++ SparkleSpin/Style/Font.swift
@@ -9,9 +9,9 @@
 import UIKit
 
 struct Font {
-    static let titleFont = UIFont(name: "LIBRARY3AM-Regular", size: 64)
+    static let titleFont = UIFont(name: "LIBRARY3AM", size: 64)
     static let decorativeFont = UIFont(name: "PoetsenOne-Regular", size: 48)
-    static let subheaderFont = UIFont(name: "saxMono-Regular", size: 40)
-    static let mainBodyFont = UIFont(name: "saxMono-Regular", size: 32)
-    static let wheelBodyFont = UIFont(name: "JUICERegular-Regular", size: 26)
+    static let subheaderFont = UIFont(name: "saxMono", size: 40)
+    static let mainBodyFont = UIFont(name: "saxMono", size: 32)
+    static let wheelBodyFont = UIFont(name: "JUICERegular", size: 26)
 }
