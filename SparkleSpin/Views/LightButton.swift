93f37d3a712ad67d6f5469da4d19abfcf6147aa4
Britney Smith <brit.smith7@gmail.com>
2019-03-05 17:12:18 -0500

    Add custom button with constraints

--- SparkleSpin/Views/LightButton.swift
+++ SparkleSpin/Views/LightButton.swift
@@ -8,27 +8,27 @@
 
 import UIKit
 
+@IBDesignable
 class LightButton: UIButton {
     
-    var buttonTitle: String?
-    
+
     override init(frame: CGRect) {
         super.init(frame: frame)
+        styleButton()
     }
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         styleButton()
-        buttonTitle = "Example"
     }
     
     private func styleButton() {
         self.backgroundColor = ThemeColor.Light.primaryColor
         self.layer.cornerRadius = 8
         self.layer.shadowColor = ThemeColor.Light.secondaryColor?.cgColor
-        self.layer.shadowOffset = CGSize(width: 0.0, height: 2.0)
-        self.layer.shadowOpacity = 1.0
-        self.layer.shadowRadius = 0.0
+        self.layer.shadowOffset = CGSize(width: 1.5, height: 2.0)
+        self.layer.shadowOpacity = 0.9
+        self.layer.shadowRadius = 2.0
         self.layer.masksToBounds = false
     }
 }
