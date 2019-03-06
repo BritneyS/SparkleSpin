3be621d3d846d5b45d8684efd3033cd410e8a5ac
Britney Smith <brit.smith7@gmail.com>
2019-03-06 15:26:46 -0500

    Refine animations

--- SparkleSpin/Views/LightButton.swift
+++ SparkleSpin/Views/LightButton.swift
@@ -22,13 +22,52 @@ class LightButton: UIButton {
         styleButton()
     }
     
+    override var isHighlighted: Bool {
+        didSet {
+            if isHighlighted {
+                isHighlighted = false
+                self.setTitleColor(ThemeColor.Light.accentColorTwo, for: .normal)
+            } else {
+                self.setTitleColor(ThemeColor.Light.secondaryColor, for: .normal)
+            }
+            
+        }
+    }
+    
+    
     private func styleButton() {
-        self.backgroundColor = ThemeColor.Light.primaryColor
-        self.layer.cornerRadius = 8
-        self.layer.shadowColor = ThemeColor.Light.secondaryColor?.cgColor
-        self.layer.shadowOffset = CGSize(width: 1.5, height: 2.0)
-        self.layer.shadowOpacity = 0.9
-        self.layer.shadowRadius = 2.0
-        self.layer.masksToBounds = false
+        
+        backgroundColor = ThemeColor.Light.primaryColor
+        layer.cornerRadius = 8
+        layer.shadowColor = ThemeColor.Light.secondaryColor?.cgColor
+        layer.shadowOffset = CGSize(width: 1.5, height: 2.0)
+        layer.shadowOpacity = 0.9
+        layer.shadowRadius = 2.0
+        layer.masksToBounds = false
+        
+        
+    }
+    
+    func animateButton() {
+
+        let animation = CABasicAnimation(keyPath: "shadowRadius")
+        animation.fromValue = 2.0
+        animation.toValue = 0.5
+        animation.duration = 0.15
+        
+        
+        let shadowAnimation = CABasicAnimation(keyPath: "shadowOffSet")
+        shadowAnimation.fromValue = CGSize(width: 1.5, height: 2.0)
+        shadowAnimation.toValue = CGSize(width: 0.0, height: 0.0)
+        shadowAnimation.duration = 0.15
+        
+        let opacityAnimation = CABasicAnimation(keyPath: "shadowOpacity")
+        opacityAnimation.fromValue = 0.9
+        opacityAnimation.toValue = 0.0
+        opacityAnimation.duration = 0.15
+        
+        layer.add(opacityAnimation, forKey: "shadowOpacity")
+        layer.add(animation, forKey: "shadowRadius")
+        layer.add(shadowAnimation, forKey: "shadowOffSet")
     }
 }
