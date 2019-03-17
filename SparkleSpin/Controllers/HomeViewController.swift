3e6ae8d946b63dbf5626d93f98c824ef3badd2e7
Britney Smith <brit.smith7@gmail.com>
2019-03-16 20:29:01 -0400

    Add navigation bar, change segue from modal to show (push)

--- SparkleSpin/Controllers/HomeViewController.swift
+++ SparkleSpin/Controllers/HomeViewController.swift
@@ -14,6 +14,16 @@ class HomeViewController: UIViewController {
         super.viewDidLoad()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+    
     
     @IBAction func startButtonIsTapped(_ sender: LightButton) {
         sender.animateButton()
