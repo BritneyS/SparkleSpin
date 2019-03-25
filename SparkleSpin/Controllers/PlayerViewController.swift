1f0c925810baec5d73ef84df80e785a921ceb9dc
Britney Smith <brit.smith7@gmail.com>
2019-03-25 13:55:17 -0400

    Refactor uitableview protocol extensions

--- SparkleSpin/Controllers/PlayerViewController.swift
+++ SparkleSpin/Controllers/PlayerViewController.swift
@@ -33,28 +33,30 @@ class PlayerViewController: UIViewController {
         playerTableView.estimatedRowHeight = CGFloat(Constants.estimatedRowHeight)
         playerTableView.tableFooterView = UIView()
     }
-
-}
-
-extension PlayerViewController: UITableViewDataSource, UITableViewDelegate {
     
     func registerNib() {
         let entryCell = UINib(nibName: NibID.entryCell, bundle: nil)
         playerTableView.register(entryCell, forCellReuseIdentifier: CellID.entryCell)
     }
+}
+
+extension PlayerViewController: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 1
     }
     
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return CGFloat(Constants.entryCellRowHeight)
+    }
+}
+
+extension PlayerViewController: UITableViewDataSource {
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let entryCell = tableView.dequeueReusableCell(withIdentifier: CellID.entryCell, for: indexPath) as? EntryCell else { fatalError("Fatal error: No cell") }
         return entryCell
     }
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return CGFloat(Constants.entryCellRowHeight)
-    }
-    
-    
 }
+
+
